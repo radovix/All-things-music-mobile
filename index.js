67eab77c6ce8dedc97efba945b919346d45dff34
Matej Radović <matej.radovic@fer.hr>
2019-01-26 23:26:27 +0100

    Applications finally runnable

---
+++ index.js
@@ -0,0 +1,9 @@
+// General imports
+//import 'es6-symbol/implement';
+import { AppRegistry } from 'react-native';
+
+// Components import
+import App from './App';
+
+// Starts the application with sentry
+AppRegistry.registerComponent('ATM', () => App)
