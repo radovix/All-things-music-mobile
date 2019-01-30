1ea50c512f53f6ca10847509c4be9492441cb8f9
Matej Radović <matej.radovic@fer.hr>
2019-01-30 12:51:02 +0100

    Artist list mock data initialized

---
+++ mocks/Artists/ArtistsListMockData.js
@@ -0,0 +1,15 @@
+/**
+ * Artists list mock data.
+ */
+const artistsList = [
+ {
+   id: 0,
+   name: 'Taylor Swift',
+   summary: 'Taylor Alison Swift je američka country-pop kantautorica i glumica. The New York Times opisao je Swift kao jednu od najboljih pop skladatelja, koja je više u kontaktu sa svojim unutrašnjim životim nego većina odraslih.',
+   genres: [1, 2],
+   image: 'http://koji.kur.ac/JCcjvg',
+   albums: []
+ } 
+]
+
+export default artistsList;
